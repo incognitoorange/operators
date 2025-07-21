0de16f487edf99265cbf9b5ca6ce240109826bd9
incognito <sylsterior@gmail.com>
2025-07-21 17:59:15 +0100

    commit 2

--- main2.py
+++ main2.py
@@ -1,12 +1,25 @@
-# Taking total amount as input from user
-Amount =int(input("Please Enter Amount for Withdraw :"))
+print("Welcome to World Bank")
+name = input("Enter your name: ")
+print("Welcome", name, "to the bank")
 
-# Calculating the number of notes of different denominations
-note_1 = Amount//100
-note_2 = (Amount%100)//50
-note_3 = ((Amount%100)%50)//10
+print("Enter the amount that you would like to withdraw")
+amount = int(input("Enter the amount: "))
 
+note1000 = amount // 1000
+note500 = (amount % 1000) // 500
+note200 = ((amount % 1000) % 500) // 200
+note100 = (((amount % 1000) % 500) % 200) // 100
+note50 = ((((amount % 1000) % 500) % 200) % 100) // 50
+note20 = (((((amount % 1000) % 500) % 200) % 100) % 50) // 20
+note10 = ((((((amount % 1000) % 500) % 200) % 100) % 50) % 20) // 10
+note5  = (((((((amount % 1000) % 500) % 200) % 100) % 50) % 20) % 10) // 5
 
-print( "notes of 100 rupee" , note_1)
-print("notes of 50 rupee" , note_2)
-print("notes of 10 rupee" , note_3)
+print("\nYou will receive:")
+print("₦1000 notes:", note1000)
+print("₦500 notes:", note500)
+print("₦200 notes:", note200)
+print("₦100 notes:", note100)
+print("₦50 notes:", note50)
+print("₦20 notes:", note20)
+print("₦10 notes:", note10)
+print("₦5 notes:", note5)
